b6ee11ff49c2fa3e79933d8b732b65013f465646
REGIE <REGIE@LAPTOP-N9QJBTF3>
2024-11-26 21:22:42 +0800

    recursion

---
+++ src/act3recursion/Act3Recursion.java
@@ -0,0 +1,26 @@
+
+package act3recursion;
+
+import java.util.Scanner;
+
+public class Act3Recursion {
+
+        public static int Print(int n){
+       if(n==0) {
+           return 0;
+       } else {
+           System.out.println("*");
+           return Print(n-1);
+       }
+        
+    }
+    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter Size: ");
+        int size = scn.nextInt();
+        Print(size);
+
+    }
+    
+}
